d5d3b081cc44d85560b6ee53f5bc3ff94bbb6b09
Gren95 <72512054+Greninja9559@users.noreply.github.com>
2022-09-29 17:25:22 +0300

    cs

--- Toolbox.ps1
+++ Toolbox.ps1
@@ -1,18 +1,16 @@
 ﻿param([switch]$Elevated)
 function Test-Admin {
-    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
-
 if ((Test-Admin) -eq $false)  {
     if ($elevated) {
-        # tried to elevate, did not work, aborting
+         'tried to elevate, did not work, aborting'
 } else {
-        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
 }
-#    exit
+exit
 }
-
 'running with full privileges'
 
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
