a11da63c7407dc975b2b6f1227457de956de617f
Gren95 <72512054+Greninja9559@users.noreply.github.com>
2022-09-30 20:38:35 +0300

    --

--- Toolbox.ps1
+++ Toolbox.ps1
@@ -1,19 +1,4 @@
-﻿#param([switch]$Elevated)
-##function Test-Admin {
-#$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-#$currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-#}
-#if ((Test-Admin) -eq $false)  {
-#    if ($elevated) {
-#         'tried to elevate, did not work, aborting'
-#} else {
-#Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-#}
-#exit
-#}
-#'running with full privileges'
-
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+﻿[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 
 $ApplicationForm = New-Object System.Windows.Forms.Form
